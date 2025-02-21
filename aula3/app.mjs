12521ade152f129de176976da3f6dfd1c4b4e1a1
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-02-21 16:08:47 -0300

    adiciona lógica para parâmetros de URL

--- aula3/app.mjs
+++ aula3/app.mjs
@@ -1,16 +1,28 @@
 import http from "node:http";
 import { stock } from "./stock.mjs";
+import { URL } from "node:url";
 
 const server = http.createServer();
 
 server.addListener("request", (request, response) => {
-  if (request.url === "/") {
+  const urlObject = new URL(`http://${request.headers.host}${request.url}`);
+
+  if (urlObject.pathname === "/") {
     response.writeHead(200, { "Content-Type": "application/json" });
     response.write(JSON.stringify(stock));
     response.end();
   }
 
-  if (request.url === "/produtosIndisponiveis") {
+  if (urlObject.pathname === "/get-by-id") {
+    const selectedObject = stock.find(
+      (product) => product.id === Number(urlObject.searchParams.get("id"))
+    );
+    response.writeHead(200, { "Content-Type": "application-json" });
+    response.write(JSON.stringify(selectedObject));
+    response.end();
+  }
+
+  if (urlObject.pathname === "/produtos-indisponiveis") {
     const unavaliableProducts = stock.filter(
       (product) => product.amountLeft == 0
     );
