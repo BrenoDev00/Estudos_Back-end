efea31e9c9ee13ff945be612d97b634114fe94bb
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-11 11:06:45 -0300

    aprendendo sobre parâmetros de rota

--- api-formula-1/app.js
+++ api-formula-1/app.js
@@ -5,9 +5,18 @@ const app = express();
 
 const baseAPIroute = "/api/v1";
 
+// o get é usado para obter o resultado de um endpoint
 app.get(baseAPIroute + "/drivers", (req, res) => {
   res.status(200).send(drivers);
-}); // o get é usado para obter o resultado de um endpoint
+});
+
+// ":position" é a sintaxe usada para parâmetros de rota
+app.get(baseAPIroute + "/drivers/standings/:position", (req, res) => {
+  const position = req.params.position;
+  const selectedDriver = drivers[position - 1];
+
+  res.status(200).send(selectedDriver);
+});
 
 const port = 3001;
 
