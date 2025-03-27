9d54d0dcba6aa3d1b3147f4c8566656a3ef57bb6
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-27 15:56:22 -0300

    refatora rotas

--- middleware/routes/home.js
+++ middleware/routes/home.js
@@ -2,6 +2,6 @@ import express from "express";
 
 export const homeRouter = express.Router();
 
-homeRouter.get("/", (req, res) => {
+homeRouter.route("/").get((req, res) => {
   res.status(200).send("<h1>Bem vindo ao site!</h1>");
 });
--- middleware/routes/posts.js
+++ middleware/routes/posts.js
@@ -3,6 +3,17 @@ import { posts } from "../data.js";
 
 export const postsRouter = express.Router();
 
-postsRouter.get("/", (req, res) => {
-  res.status(200).send(posts);
-});
+postsRouter
+  .route("/")
+  .get((req, res) => {
+    res.status(200).send(posts);
+  })
+  .post((req, res) => {
+    res.status(200).send("usuário criado!");
+  })
+  .put((req, res) => {
+    res.status(200).send("usuário editado!");
+  })
+  .delete((req, res) => {
+    res.status(200).send("usuário excluído!");
+  });
