a5a978ab9022a17052c64dc61285d67db610e90c
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-29 15:40:16 -0300

    adiciona o helmet na API

--- middleware/app.js
+++ middleware/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { homeRouter } from "./routes/home.js";
 import { postsRouter } from "./routes/posts.js";
+import helmet from "helmet";
 
 // Middlewares são algoritmos ou trechos de código que atuam entre uma requisição e resposta HTTP.
 
@@ -10,24 +11,26 @@ const app = express();
 app.use(registerRequest);
 app.use(registerOnDatabase);
 
+// O helmet é um middleware que ajuda proteger a API contra vulnerabilidades relacionadas ao cabeçalho HTTP.
+app.use(helmet());
+
 app.use("/", homeRouter);
 app.use("/posts", postsRouter);
 
-
 // Essa função representa um middleware
 function registerRequest(req, res, next) {
-    console.log("nova solicitação HTTP");
-    console.log("endpoint solicitado: ", req.url);
-    
-    next();
+  console.log("nova solicitação HTTP");
+  console.log("endpoint solicitado: ", req.url);
+
+  next();
 }
 
 // Essa função representa um middleware
 function registerOnDatabase(req, res, next) {
-    console.log("Acessando o banco de dados");
-    console.log("Registrando transação");
-    
-    next();
+  console.log("Acessando o banco de dados");
+  console.log("Registrando transação");
+
+  next();
 }
 
 const port = 3001;
