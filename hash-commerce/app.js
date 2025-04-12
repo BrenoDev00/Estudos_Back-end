4e59135ecce9729b9206a84eff137bc8692b9bf8
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-04-12 20:54:18 -0300

    adiciona conexão com banco de dados

--- hash-commerce/app.js
+++ hash-commerce/app.js
@@ -1,1 +1,11 @@
-console.log("ola mundo");
+import pg from "pg";
+
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "root",
+  database: "hash-commerce",
+});
+
+console.log(await pool.query("SELECT * FROM public.users"));
