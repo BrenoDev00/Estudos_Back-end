c456680cfb91228a7180f67b5ed9edca0d2953be
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-04-16 14:56:12 -0300

    adiciona requisição get

--- hash-commerce/app.js
+++ hash-commerce/app.js
@@ -1,4 +1,13 @@
 import "dotenv/config";
 import { pool } from "./data-base.js";
+import express from "express";
 
-console.log((await pool.query("SELECT * FROM public.users")).rows);
+const app = express();
+
+app.get("/", async (req, res) => {
+  const result = (await pool.query("SELECT * FROM public.users")).rows;
+
+  res.status(200).send(result);
+});
+
+app.listen(3001, () => console.log("API  rodando na porta 3001"));
