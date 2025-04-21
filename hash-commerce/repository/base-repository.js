8095a58f28ef8bf188c2a30c2f3edfe5b38dadbc
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-04-21 19:44:20 -0300

    adiciona método createData

--- hash-commerce/repository/base-repository.js
+++ hash-commerce/repository/base-repository.js
@@ -22,4 +22,26 @@ export class BaseRepository {
       throw error;
     }
   }
+
+  async createData(table, values, columns) {
+    const poolConection = pool.connect();
+    try {
+      const flags = Array.from(new Array(columns.length).keys());
+
+      const formatedFlags = flags.map((flag) => `$${flag + 1}`);
+
+      const query = `INSERT INTO public.${table} (${columns.join(
+        ", "
+      )}) VALUES (${formatedFlags.join(", ")})`;
+
+      (await poolConection).query("BEGIN TRANSACTION");
+      (await poolConection).query(query, values);
+      (await poolConection).query("COMMIT");
+    } catch (error) {
+      (await poolConection).query("ROLLBACK");
+      throw error;
+    } finally {
+      (await poolConection).release();
+    }
+  }
 }
