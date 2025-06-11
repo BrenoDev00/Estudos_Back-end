d9a08e2a7e375952e2337a589813e518a783a954
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-11 15:59:51 -0300

    adiciona helmet

--- api-pets/src/app.ts
+++ api-pets/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import { petRouter } from "./routes/pet-routes.js";
+import helmet from "helmet";
 
 const app: Express = express();
+
+app.use(helmet());
 
 app.use(express.json());
 
 app.use("/pets", petRouter);
-
 
 export default app;
