43b3a34612f75dd2bbab24b936c577138cfcee13
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-17 21:27:16 -0300

    adiciona tabela tasks

--- prisma/src/app.ts
+++ prisma/src/app.ts
@@ -2,7 +2,9 @@ import { prisma } from "./config/data-base";
 
 async function main() {
   const users = await prisma.user.findMany();
+  const tasks = await prisma.task.findMany();
   console.log(users);
+  console.log(tasks);
 }
 
 main()
