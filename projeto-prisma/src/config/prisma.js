175ea30da9f5492446a609c8c96e6d0e64388305
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-09-10 20:15:37 -0300

    ajusta prisma client

--- projeto-prisma/src/config/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-var prisma_1 = require("../../generated/prisma");
-exports.prisma = new prisma_1.PrismaClient();
