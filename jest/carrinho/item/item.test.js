44e5c722b29d91cb8dc13a74933d9dc50c9d9596
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-01-14 10:54:35 -0300

    adiciona testes unitários para Item

--- jest/carrinho/item/item.test.js
+++ jest/carrinho/item/item.test.js
@@ -1,3 +1,16 @@
+import Item from "./item"
+
 describe("Teste dos itens", () => {
-    Item("")
+    it("Deve ter 3 campos: nome, valor e quantidade", () => {
+        const item = new Item("Beterraba", 2.5, 10);
+
+        expect(item.nome).toBe("Beterraba");
+        expect(item.quantidade).toBe(10);
+        expect(item.valor).toBe(2.5);
+    });
+
+    it("Deve ter o preço calculado de acordo com a quantidade", () => {
+        const item = new Item("Laranja", 4.5, 6);
+        expect(item.pegaValorTotalItem()).toBe(27);
+    })
 })
