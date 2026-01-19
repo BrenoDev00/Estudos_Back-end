759d8abd765e143fd2f3fb19a738f2e7809c10e8
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-01-19 20:42:22 -0300

    finaliza testes

--- api-jest/src/models/editora/editora.test.js
+++ api-jest/src/models/editora/editora.test.js
@@ -1,4 +1,5 @@
 import Editora from "./editora.js";
+import Livro from "../livro.js";
 
 describe("Editora testes unitários", () => {
   const objetoEditora = {
@@ -50,5 +51,63 @@ describe("Editora testes unitários", () => {
     );
   });
 
-  
+  it("Deve permitir editar uma editora cadastrada", async () => {
+    const editora = new Editora({ id: 1, ...objetoEditora });
+
+    await editora.salvar();
+
+    const editoraBuscada = await Editora.pegarPeloId(1);
+
+    expect(editoraBuscada).toEqual(
+      expect.objectContaining({
+        id: 1,
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
+
+  it("Deve permitir excluir uma editora", async () => {
+    const editora = new Editora({ id: 1, ...objetoEditora });
+
+    await editora.salvar();
+
+    await Editora.excluir(1);
+
+    expect(await Editora.pegarPeloId(1)).toEqual(
+      expect.not.objectContaining({
+        id: 1,
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
+
+  it("Deve permitir listar livros por editora", async () => {
+    const editoraSalva = new Editora({ id: 2, ...objetoEditora });
+
+    const livro = new Livro({
+      id: 4,
+      titulo: "O Feiticeiro de Terramar",
+      paginas: 450,
+      editora_id: 2,
+      autor_id: 2,
+      created_at: "2022-07-01 19:49:06",
+      updated_at: "2022-07-01 19:49:06",
+    });
+
+    const livroSalvo = await livro.criar();
+
+    const livrosPorEditora = await Editora.pegarLivrosPorEditora(
+      editoraSalva.id,
+    );
+
+    const livrosEsperados = [livroSalvo];
+
+    expect(livrosPorEditora.livros).toEqual(
+      expect.arrayContaining(livrosEsperados),
+    );
+  });
 });
